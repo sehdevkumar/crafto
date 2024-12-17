fae0f272acae7908567cb6315f4bb3f86c25e122
sehdevkumar <kumarsehdev600@gmail.com>
2024-12-17 21:46:52 +0530

    added

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -6,34 +6,50 @@ import { QUOTES_PATH } from '../../constants/env';
 import { QuoteApiResponse } from '../../types/api-response-typings';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-quotes',
   standalone: true,
-  imports: [MatPaginatorModule,CommonModule],
+  imports: [MatPaginatorModule, CommonModule,MatIconModule,MatButtonModule],
   templateUrl: './quotes.component.html',
   styleUrl: './quotes.component.scss'
 })
 export class QuotesComponent implements OnInit {
   
+  as = inject(AuthService);
+  router = inject(Router);
+  http = inject(HttpClient);
 
-  as = inject(AuthService)
-  router = inject(Router)
-  http = inject(HttpClient)
+  quotes = signal<QuoteApiResponse[]>([]);
+  limit: number = 10; // Number of quotes per page
+  offset: number = 0; // Starting point for fetching quotes
 
-  quotes = signal<QuoteApiResponse[]>([])
-
-  
   ngOnInit(): void {
-     
-    this.http.get(QUOTES_PATH,{
+    this.fetchQuotes();
+  }
+
+  fetchQuotes(): void {
+
+    this.http.get(QUOTES_PATH + `?limit=${this.limit}&offset=${this.offset}`, {
       headers: {
         'Authorization': `${this.as.getToken()}`
       }
-    }).subscribe((res:any) => {
-      this.quotes.set(res?.data as QuoteApiResponse[])
+    }).subscribe((res: any) => {
+      this.quotes.set(res?.data as QuoteApiResponse[]);
     });
   }
 
+  onPageChange(event: any): void {
+    this.offset = event.pageIndex * this.limit; // Calculate new offset
+    this.limit = event.pageSize;
+    this.fetchQuotes(); // Fetch quotes for the new page
+  }
 
+  refreshQuotes(): void {
+    this.offset = 0;
+    this.limit = 10;
+    this.fetchQuotes();
+  }
 }
